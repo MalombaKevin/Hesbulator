32227810f296a3e5467d4c9b3e68ab5d303b2ff8
Kevin Malomba <malombakevin@gmail.com>
2025-04-19 01:48:29 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function appendValue(value) {
+  document.getElementById('display').value += value;
+}
+
+function clearDisplay() {
+  document.getElementById('display').value = '';
+}
+
+
+function calculate() {
+  
+    const result = eval(document.getElementById('display').value);
+    document.getElementById('display').value = result;
+  
+}
